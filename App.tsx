75b854fd4777902745c8b4057310444f1fd1edf4
Williams Bolade <dbolade2000@gmail.com>
2022-08-27 17:17:15 +0100

    OnBoard Pages now swiping

--- App.tsx
+++ App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import AccountScreen from './src/screens/AccountScreen';
-import GeneralOverviewScreen from './src/screens/GeneralOverviewScreen';
-import OnboardingScreens from './src/screens/OnboardingScreens';
-
-const Stack = createNativeStackNavigator();
+import OnboardStackNavigator from './src/navigation/OnboardStackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          component={OnboardingScreens}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="My Account" component={AccountScreen} />
-        <Stack.Screen
-          name="General Overview"
-          component={GeneralOverviewScreen}
-        />
-      </Stack.Navigator>
+      <OnboardStackNavigator />
     </NavigationContainer>
   );
 };
