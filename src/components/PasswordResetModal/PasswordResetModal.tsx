91dfbb4a39b9f36e0ce42773276d280ade8d193c
Williams Bolade <dbolade2000@gmail.com>
2022-09-06 15:53:48 +0100

    Overview Screen

--- src/components/PasswordResetModal/PasswordResetModal.tsx
+++ src/components/PasswordResetModal/PasswordResetModal.tsx
@@ -1,4 +1,7 @@
 import React, {FC} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AuthenticationNavigatorParams} from '../../navigation/AuthenticationNavigator';
 import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './PasswordResetModal.styles';
 
@@ -6,6 +9,8 @@ interface IPasswordReset {
   email: string;
 }
 const PasswordResetModal: FC<IPasswordReset> = ({email}) => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AuthenticationNavigatorParams>>();
   return (
     <TouchableOpacity style={styles.container}>
       <View style={styles.modal}>
@@ -14,7 +19,13 @@ const PasswordResetModal: FC<IPasswordReset> = ({email}) => {
           <Text style={styles.modalText}>
             Email has been sent to {email} with a link to reset password
           </Text>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              navigation.navigate('AuthenticatorNavigator', {
+                screen: 'Login',
+              });
+            }}>
             <Text style={styles.buttonText}>OK</Text>
           </TouchableOpacity>
         </View>
---
+++ src/screens/OverviewScreen/OverviewScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+import styles from './OverviewScreen.styles';
+
+const OverviewScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default OverviewScreen;
