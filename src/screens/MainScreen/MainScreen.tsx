a8f6feb8bc89624e986db546ad781a05df6f1867
Williams Bolade <dbolade2000@gmail.com>
2022-09-08 00:54:01 +0100

    Main Card being implemented

--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import styles from './MainScreen.styles';
 import ProfileAvatar from '../../components/ProfileAvatar/ProfileAvatar';
 
+const cardImg = '../../assets/images/home.png';
 const MainScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.profileBar}>
         <Text style={styles.profileText}>Hi, Chidera</Text>
         <View style={styles.headerStyle}>
-          <Icon name="notifications-outline" size={30} />
+          <View style={styles.iconView}>
+            <Icon name="notifications-outline" size={30} />
+          </View>
           <ProfileAvatar />
         </View>
+      </View>
+      <View>
+        <Image source={require(cardImg)} style={styles.cardImage} />
+        <Text style={styles.cardText}>Your total points</Text>
       </View>
     </View>
   );
