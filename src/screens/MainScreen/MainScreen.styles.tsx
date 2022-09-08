ce6e2392f6d8947062bd0b653d3ba543e04f73ee
Williams Bolade <dbolade2000@gmail.com>
2022-09-08 01:16:12 +0100

    card completed

--- src/screens/MainScreen/MainScreen.styles.tsx
+++ src/screens/MainScreen/MainScreen.styles.tsx
@@ -39,6 +39,39 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     color: 'white',
   },
+  coinImage: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    position: 'absolute',
+    marginTop: 70,
+    marginLeft: 20,
+  },
+  pointsText: {
+    position: 'absolute',
+    marginTop: 75,
+    marginLeft: 70,
+    color: 'orange',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  redeemButton: {
+    color: 'white',
+    position: 'absolute',
+    marginTop: 130,
+    marginLeft: 20,
+    backgroundColor: 'white',
+    height: 40,
+    width: 100,
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  redeemButtonText: {
+    fontWeight: '500',
+    color: TextColor,
+    fontSize: 13,
+  },
 });
 
 export default styles;
--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import styles from './MainScreen.styles';
 import ProfileAvatar from '../../components/ProfileAvatar/ProfileAvatar';
 
 const cardImg = '../../assets/images/home.png';
+const coinImg = '../../assets/images/coin.png';
 const MainScreen = () => {
   return (
     <View style={styles.container}>
@@ -20,6 +21,12 @@ const MainScreen = () => {
       <View>
         <Image source={require(cardImg)} style={styles.cardImage} />
         <Text style={styles.cardText}>Your total points</Text>
+        <Image source={require(coinImg)} style={styles.coinImage} />
+        <Text style={styles.pointsText}>4040405040404</Text>
+        <TouchableOpacity style={styles.redeemButton}>
+          <Text style={styles.redeemButtonText}>Redeem Points</Text>
+        </TouchableOpacity>
+        <View></View>
       </View>
     </View>
   );
